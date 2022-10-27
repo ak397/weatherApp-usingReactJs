e155e718b9d6708be8279cf0b24ea851505979cf
ak397 <ak397868@gmail.com>
2022-10-27 19:07:44 +0530

    night mode ,c to f

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import "./App.css";
-import WeatherUpdate from "./components/WeatherUpdate";
-
+import "./App.css"
+import CopyWeatherUpdate from "./components/CopyWeatherUpdate"
+// import WeatherUpdate from "./components/WeatherUpdate";//main code work
 
 // import HourlyUpdate from "./components/HourlyUpdate";
 // import Dummy from "./components/Dummy";
 
-const App=()=> {
-    return (
-      <>
-        <WeatherUpdate/>
-        {/* <HourlyUpdate/> */}
-       {/* <Dummy/> */}
-      </>
-    );
+const App = () => {
+  return (
+    <>
+      {/* <WeatherUpdate/>//main code work */}
+      {/* <HourlyUpdate/> */}
+      {/* <Dummy/> */}
+      <CopyWeatherUpdate />
+    </>
+  )
 }
-export default App;
+export default App
