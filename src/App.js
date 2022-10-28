b6ddfb043c34b9687b86606f7326fd06bd02db6b
ak397 <ak397868@gmail.com>
2022-10-28 21:36:33 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import CopyWeatherUpdate from "./components/CopyWeatherUpdate"
 import { MadeWithLove } from "./components/MadeWithLove"
 // import WeatherUpdate from "./components/WeatherUpdate";//main code work
 
-// import HourlyUpdate from "./components/HourlyUpdate";
-// import Dummy from "./components/Dummy";
-
 const App = () => {
+
   return (
     <>
       {/* <WeatherUpdate/>//main code work */}
-      {/* <HourlyUpdate/> */}
-      {/* <Dummy/> */}
       <CopyWeatherUpdate />
       <MadeWithLove/>
     </>
